a7189b59c94a20a51e2c51aa3a18a00c84f9d3db
Guarinboss <cenefo2706@gmail.com>
2021-11-29 16:30:37 -0500

    interfaz de inicio

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { jsPDF } from "jspdf";
+
+// Default export is a4 paper, portrait, using millimeters for units
+const doc = new jsPDF();
 
 
 export interface Album {
@@ -33,14 +37,35 @@ export class CarritoComponent implements OnInit {
 
 ];
 
+
   constructor(fb: FormBuilder) { 
     this.toppings = fb.group({
       pepperoni: false,
     });
+    
+  }
 
+  public downloadPDF(): void {
+    const doc = new jsPDF();
+
+    doc.text('FACTURA', 90, 10);
+    doc.text('Subtotal .......................................................................', 25, 30);
+    doc.text('IVA ...............................................................................', 25, 40);
+    doc.text('TOTAL .........................................................................', 25, 55);
+    doc.text('15.000$', 160, 30);
+    doc.text('3.000$', 160, 40);
+    doc.text('18.000$', 160, 55);
+    doc.text('DASMusic', 170, 290);
+    doc.text('Gracias por su compra, ', 25, 275);
+    doc.text('David Caballero.', 85, 275);
+    doc.save('factura.pdf');
   }
 
   ngOnInit(): void {
+  }
+
+  comprarCarrito(){
+    this.downloadPDF();
   }
 
 }
